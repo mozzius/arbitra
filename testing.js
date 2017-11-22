c66d55dc96896e075408a7b8224ac5a7b83bd764
Mozzius <mozzius.iii@gmail.com>
2017-11-22 12:55:15 +0000

    removed old files
    testing.html and testing.js were old files before I made the page system. They are no longer necessary.

--- testing.js
+++
@@ -1,8 +0,0 @@
-const remote = require('electron').remote
-
-function sha256 (data) {
-  	// creates a sha256 hash, updates it with data, and turns it into a hexadecimal string
-    var hash = crypto.createHash('sha256').update(data).digest("hex")
-    return hash
-}
-
