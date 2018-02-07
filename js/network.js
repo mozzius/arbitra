fc7fa39bc3e697a21e42d252c0e024e4edbbda06
Mozzius <mozzius.iii@gmail.com>
2018-02-07 16:18:31 +0000

    update idk

--- js/network.js
+++ js/network.js
@@ -43,8 +43,8 @@ function parseMsg2(data,callback) {
             if (!result) {
                 throw 'signature'
             } else {
-                //verifyAmount(msg.body.from)
-                throw 'dunno'
+                //need to check that they have enough au
+                //blockchain.verifyAmount(msg.body.from,msg.body.amount)
             }
         })
     }
@@ -56,7 +56,7 @@ function parseMsg2(data,callback) {
     }
     try {
         var msg = JSON.parse(data)
-        if (msg.header.hash === hash.sha256hex(JSON.stringify(msg.body))) {
+        if (msg.header.hash === hash.sha256hex(JSON.stringify(msg.body)+msg.header.time)) {
             if (msg.header.type === 'tx') {
                 tx(msg)
             } else if (msg.header.type === 'bk') {
@@ -85,7 +85,7 @@ function parseMsg2(data,callback) {
         }
     } finally {
         reply.header.time = Date.now()
-        reply.header.hash = hash.sha256hex(JSON.stringify(reply.body))
+        reply.header.hash = hash.sha256hex(JSON.stringify(reply.body)+reply.header.time)
         reply.header.size = Buffer.byteLength(JSON.stringify(reply.body,'utf8'))
         var replystr = JSON.stringify(reply)
         callback(replystr)
@@ -95,6 +95,7 @@ function parseMsg2(data,callback) {
 function sendMsg(message,ip) {
     var client = new net.Socket()
     client.connect(2018,ip,() => {
+        console.log('Connected to: '+ip)
         client.write(message)
         client.on('data',(data) => {
             console.log('Client received: '+data)
--- js/testing.js
+++ js/testing.js
@@ -1,9 +1,9 @@
-const ecdsa = require('./ecdsa.js')
-const bigInt = require('big-integer')
 const network = require('./network.js')
 
 function init() {
     console.log('testing.js loaded')
+    var ip = '109.157.157.179'
+    network.sendMsg('hello world',ip)
 }
 
 exports.init = init
