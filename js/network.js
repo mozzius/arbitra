b1851b5591cf3136eb863e1a846b8a1f06f54365
Mozzius <mozzius.iii@gmail.com>
2018-02-15 15:23:09 +0000

    parsereply

--- js/network.js
+++ js/network.js
@@ -22,6 +22,54 @@ function init() {
     // should be 2018
     server.listen(2018)
     console.log('Server started')
+
+    // start trying to connect to other nodes
+    file.getAll('connections',(data) => {
+        var connections = JSON.parse(data)
+        var ping = {
+            "header": {
+                "type": "pg"
+            },
+            "body": {}
+        }
+        file.get('advertise','network-settings',(data) => {
+            var advertise = JSON.parse(data)
+            ping.body['advertise'] = advertise
+            connections.forEach((node) => {
+                sendMsg(ping,node.ip,() => {
+                    
+                })
+            })
+        })
+    })
+}
+
+function sendMsg(msg,ip,callback) {
+    msg.body['time'] = Date.now()
+    msg.header['version'] = version
+    msg.header['hash'] = hash.sha256hex(JSON.stringify(msg.body))
+    msg.header['size'] = Buffer.byteLength(JSON.stringify(msg.body))
+    var sendMe = JSON.stringify(msg)
+    var client = new net.Socket()
+    client.connect(2018,ip,() => {
+        console.log('Connected to: '+ip)
+        client.write(msg)
+        client.on('data',(data) => {
+            console.log('Client received: '+data)
+            parseReply(data,() => {
+                // call the callback, if it exists
+                typeof callback === 'function' && callback()
+                client.destroy()
+            })
+        })
+        client.on('close',() => {
+            console.log('Connection closed')
+        })
+        client.on('timeout',() => {
+            console.warn('Client timed out')
+            client.destroy()
+        })
+    })
 }
 
 function parseMsg(data,callback) {
@@ -39,10 +87,12 @@ function parseMsg2(data,callback) {
             if (msg.header.type === 'tx') {
                 reply = parse.tx(msg)
                 // send on to other nodes
+                sendOn(msg)
                 file.append('sent',msg.header.hash,() => {})
             } else if (msg.header.type === 'bk') {
                 reply = parse.bk(msg)
                 // send on to other nodes
+                sendOn(msg)
                 file.append('sent',msg.header.hash,() => {})
             } else if (msg.header.type === 'hr') {
                 reply = parse.hr(msg)
@@ -72,35 +122,40 @@ function parseMsg2(data,callback) {
             reply.body['error'] = e
         }
     } finally {
-        reply.body['time'] = Date.now()
-        reply.header['version'] = version
-        if (reply.header.type !== 'ok') {
-            reply.header['hash'] = hash.sha256hex(JSON.stringify(reply.body)+reply.header.time)
-            reply.header['size'] = Buffer.byteLength(JSON.stringify(reply.body,'utf8'))
-        }
-        var replystr = JSON.stringify(reply)
-        console.log('Reply: '+replystr)
+        // replies with something, even if its an error
+        console.log('Reply: '+JSON.stringify(reply))
         callback(replystr)
     }
 }
 
-function sendMsg(message,ip) {
-    var client = new net.Socket()
-    client.connect(2018,ip,() => {
-        console.log('Connected to: '+ip)
-        client.write(message)
-        client.on('data',(data) => {
-            console.log('Client received: '+data)
-            client.destroy()
-        })
-        client.on('close',() => {
-            console.log('Connection closed')
-        })
-        client.on('timeout',() => {
-            console.warn('Client timed out')
-            client.destroy()
-        })
-    })
+function parseReply(data) {
+    // parse incoming messages and crafts a reply
+    // by calling parse functions
+    var reply
+    try {
+        var msg = JSON.parse(data)
+        if (msg.header.hash === hash.sha256hex(JSON.stringify(msg.body))) {
+            if (msg.header.type === 'bl') {
+                reply = parse.bl(msg)
+            } else if (msg.header.type === 'bh') {
+                reply = parse.bh(msg)
+            } else if (msg.header.type === 'nr') {
+                reply = parse.nr(msg)
+            } else if (msg.header.type === 'nd') {
+                reply = parse.nd(msg)
+            } else if (msg.header.type === 'ok') {
+                console.info('message recieved ok')
+            } else {
+                throw 'type'
+            }
+        } else {
+            throw 'hash'
+        }
+    } catch(e) {
+        console.warn('Reply error: '+e)
+    } finally {
+        callback(replystr)
+    }
 }
 
 exports.init = init
