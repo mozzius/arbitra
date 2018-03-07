9d03993408edf712a38e4d45806b8277aad9c251
Mozzius <mozzius.iii@gmail.com>
2018-03-07 16:17:08 +0000

    clear cache

--- js/pages/app-settings.js
+++ js/pages/app-settings.js
@@ -1,6 +1,14 @@
 const file = require('../file.js')
 
 function init() {
+    document.getElementById('clear').addEventListener('click',() => {
+        file.storeAll('blockchain',[])
+        file.storeAll('connections',[])
+        file.storeAll('network-settings',{"advertise":"true","target-connections":5})
+        file.storeAll('recent-connections',[])
+        file.storeAll('txpool',[])
+        document.getElementById('ca-save').classList.remove('hidden')
+    })
 }
 
 exports.init = init
